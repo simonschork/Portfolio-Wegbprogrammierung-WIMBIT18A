faf1528baf23882aa5b6ddf7478b73a40c770828
simonschork <simon.schork@gmail.com>
2019-11-05 14:22:58 +0100

    Fixed nav sidebar by changing container and css

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -12,34 +12,35 @@
   $ = $ && $.hasOwnProperty('default') ? $['default'] : $;
   Popper = Popper && Popper.hasOwnProperty('default') ? Popper['default'] : Popper;
 
-  // To Top Button:
-  $(document).ready(function() {
-    $(window).scroll(function () {
-      if ($(this).scrollTop() > 50) {
-        $('#topbtn').fadeIn();
-      } else {
-        $('#topbtn').fadeOut();
-      }
-    });
-  });
-  // scroll body to 0px on click
-  $('#topbtn').click(function () {
-    $('#topbtn').tooltip('hide');
-    $('body,html').animate({
-      scrollTop: 0
-    }, 800);
-    return false;
-  });
+  //Get the button:
+  mybutton = document.getElementById("topBtn");
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction()};
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
 
   //Sidebar
   //Scrollspy
-  $('body').scrollspy({ target: '#sidebar' })
+  $('body').scrollspy({ 
+    target: '#sidebar' 
+  })
 
-  /*event wenn higlited by scrollspy
   $('[data-spy="scroll"]').on('activate.bs.scrollspy', function () {
     // do something…
   })
-  */
 
   function _defineProperties(target, props) {
     for (var i = 0; i < props.length; i++) {
