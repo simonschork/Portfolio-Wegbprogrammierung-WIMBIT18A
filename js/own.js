67af49c06cdeccde28671b1e425d3818d06bb12e
simonschork <simon.schork@gmail.com>
2019-12-17 10:04:55 +0100

    implemented toxicity detection

--- js/own.js
+++ js/own.js
@@ -31,7 +31,48 @@ $(document).ready(function () {
 *Implementation of the functions
 */
 //Kommentar senden
-function kommentarSenden() {
+async function kommentarSenden() {
     var kommentartext = document.getElementById('commentinput').value;
-    document.getElementById('commentoutput').innerHTML = kommentartext;
+    var kommentar = Array(kommentartext);
+
+    /*
+    *Toxicity detection
+    */
+    //Minimale Vorhersagesicherheit
+    const grenzwert = 0.9;
+    //Objekt für Klassifizierung deklarieren
+    var classification = new Object();
+
+    //Model laden
+    classification = await toxicity.load(grenzwert).then(async model => {
+        //Kommentar als modelinput
+        var saetze = kommentar;
+        var modeloutput = new Object();
+        //Vorhersage
+        modeloutput = await model.classify(saetze);
+        //Vorhersage übergeben
+        return modeloutput;
+    });
+
+    console.log(classification);
+
+    //Toxicity prüfen
+    if (classification[0].results[0].match == true) {
+        alert('Entschuldigung, Ihr Kommentar wird nicht veröffentlicht, da er als Beleidigung interpretiert werden könnte!')
+    } else if (classification[1].results[0].match == true) {
+        alert('Entschuldigung, Ihr Kommentar wird nicht veröffentlicht, da er als Beleidigung interpretiert werden könnte!')
+    } else if (classification[2].results[0].match == true) {
+        alert('Entschuldigung, Ihr Kommentar wird nicht veröffentlicht, da er als Beleidigung interpretiert werden könnte!')
+    } else if (classification[3].results[0].match == true) {
+        alert('Entschuldigung, Ihr Kommentar wird nicht veröffentlicht, da er als Beleidigung interpretiert werden könnte!')
+    } else if (classification[4].results[0].match == true) {
+        alert('Entschuldigung, Ihr Kommentar wird nicht veröffentlicht, da er als Beleidigung interpretiert werden könnte!')
+    } else if (classification[5].results[0].match == true) {
+        alert('Entschuldigung, Ihr Kommentar wird nicht veröffentlicht, da er als Beleidigung interpretiert werden könnte!')
+    } else if (classification[6].results[0].match == true) {
+        alert('Entschuldigung, Ihr Kommentar wird nicht veröffentlicht, da er als Beleidigung interpretiert werden könnte!')
+    } else {
+        //Kommentar veröffentlichen
+        document.getElementById('commentoutput').innerHTML = kommentar;
+    }
 }
